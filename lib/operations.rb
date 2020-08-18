bade789f58443b6e55049ba0cf2996e8a896db5c
Max Zander <maxjacobzander@gmail.com>
2020-08-18 15:53:53 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,7 +2,9 @@ def unsafe?(speed)
 
 end
 
-
+rspec ./spec/operations_spec.rb:6 # Operations #unsafe? returns true if speed is greater than 60
+rspec ./spec/operations_spec.rb:11 # Operations #unsafe? returns true if speed is less than 40
+rspec ./spec/operations_spec.rb:16 # Operations #unsafe? returns false if the speed is between 40 and 60
 
 def not_safe?(speed)
 	
